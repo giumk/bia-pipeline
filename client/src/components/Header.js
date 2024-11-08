28aba08119eb92ed937bc435a8a800f1b13c3434
giumk <giullyanmk@hotmail.com>
2024-11-07 23:54:22 -0300

    Mudando para ingles

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,7 +11,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {location.pathname === "/" && (
         <Button
           color={showAdd ? "red" : "blue"}
-          text={showAdd ? "Fechar" : "Add"}
+          text={showAdd ? "Closed" : "Add"}
           onClick={onAdd}
         />
       )}
